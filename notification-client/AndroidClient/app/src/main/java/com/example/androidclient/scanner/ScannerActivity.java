571cbe417b423f17d10fbf4e8aa3a59be9aaad41
Alexander Ertl <ertl@student.tugraz.at>
2020-05-31 21:11:04 +0200

    qr code scanner

---
+++ notification-client/AndroidClient/app/src/main/java/com/example/androidclient/scanner/ScannerActivity.java
@@ -0,0 +1,73 @@
+package com.example.androidclient.scanner;
+
+import com.example.androidclient.R;
+import com.google.zxing.Result;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_scanner);
+        //setupToolbar();
+
+
+
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
+        scannerView = new ZXingScannerView(this);
+        contentFrame.addView(scannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        System.out.println("Resuming: Starting camera");
+        scannerView.startCamera();
+        scannerView.setResultHandler(this);
+    }
+
+    @Override
+    public void onPause() {
+        System.out.println("Pausing: Stopping camera");
+        scannerView.stopCamera();
+        super.onPause();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse(rawResult.getText()));
+        setResult(RESULT_OK, intent);
+        finish();
+
+        //TODO user confirmation
+        /*
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scannerView.resumeCameraPreview(ScannerActivity.this);
+            }
+        }, 2000);*/
+    }
+}
